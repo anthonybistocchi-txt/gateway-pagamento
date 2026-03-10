a45725b86483671ad439df4b74ab5fe460ec92a9
anthonybistocchi-txt <anthonybistocchi.txt@gmail.com>
2026-03-10 15:19:23 -0300

    feat: created migrations and models

---
+++ database/migrations/2026_03_10_125139_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+            $table->foreignId('gateway_id')->constrained()->onDelete('cascade');
+            $table->string('external_id')->nullable();
+            $table->enum('status', ['pending', 'completed', 'failed','refunded'])->default('pending');
+            $table->integer('amount');
+            $table->char('card_last_numbers', 4);
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
+use App\Models\Gateway;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +19,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::create([
+            'name' => 'Admin BeTalent',
+            'email' => 'admin@betalent.tech',
+            'password' => Hash::make('password123'),
+            'role' => 'admin',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Gateway::create([
+            'name' => 'Gateway 1',
+            'is_active' => true,
+            'priority' => 1, 
+        ]);
+
+        Gateway::create([
+            'name' => 'Gateway 2',
+            'is_active' => true,
+            'priority' => 2, 
+        ]);
+
+        Client::create([
+            'name' => 'Cliente Teste',
+            'email' => 'cliente@teste.com',
+        ]);
+
+        Product::create([
+            'name' => 'Teclado Mecânico',
+            'amount' => 35000, // R$ 350,00
+        ]);
+
+        Product::create([
+            'name' => 'Mouse Sem Fio',
+            'amount' => 12050, // R$ 120,50
         ]);
     }
 }
