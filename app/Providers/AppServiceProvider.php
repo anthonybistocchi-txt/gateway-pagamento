35140de479861e3e1b738507aff867561b3a8a7e
anthonybistocchi-txt <anthonybistocchi.txt@gmail.com>
2026-03-11 23:17:59 -0300

    register interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Interfaces\ProductRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\TransactionRepository;
+use App\Interfaces\TransactionRepositoryInterface;
+use App\Repositories\ProductRepository;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TransactionRepositoryInterface::class, TransactionRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+
     }
 
     /**
