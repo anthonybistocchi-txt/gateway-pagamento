bb370ee2da541bd52b90204850bb513177335c1b
anthonybistocchi-txt <anthonybistocchi.txt@gmail.com>
2026-03-11 23:17:39 -0300

    feat: add new route for to buy products

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+
 use App\Http\Controllers\PurchaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/newPurchase', [PurchaseController::class, 'newPurchase'])->name('newPurchase');
+Route::post('/purchases', [PurchaseController::class, 'store'])->name('store');
