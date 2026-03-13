d65509ea475c8bb9efca3eab3ca39515f11b3a5a
Anthony <anthony.bistocchi@4x4brasil.com.br>
2026-03-13 14:25:36 -0300

    refac: removed roles column at table users transfer for a new table roles

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['ADMIN ', 'MANAGER ', 'FINANCE', 'USER '])->default('USER ');
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
