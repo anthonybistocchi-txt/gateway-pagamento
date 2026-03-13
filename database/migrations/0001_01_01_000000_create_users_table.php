9cf0c8d771605a7b03c6d7aaf8128ba7449e7b74
Anthony <anthony.bistocchi@4x4brasil.com.br>
2026-03-13 14:18:31 -0300

    feat: add new roles for users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('role', ['ADMIN ', 'MANAGER ', 'FINANCE', 'USER '])->default('USER ');
             $table->rememberToken();
             $table->timestamps();
         });
